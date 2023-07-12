ea70a353fe4fb7fd50d6a3b11e728020b5f64a06
iladsuma <67311820+iladsuma@users.noreply.github.com>
2023-07-12 10:51:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,8 @@ function toggleHiddenContent() {
     hiddenContent.style.display = "block";
   }
 }
+window.addEventListener('DOMContentLoaded', function() {
+  var bgm = document.getElementById('bgm');
+  bgm.play();
+});
+
